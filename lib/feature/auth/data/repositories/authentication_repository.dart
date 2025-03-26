c056c6e7c3ef9828c1c237729fd4e35ebdd60cdb
MeZeksan <bulzak74848@gmail.com>
2025-03-26 23:46:09 +0300

    Метод входа с помощью email

--- lib/feature/auth/data/repositories/authentication_repository.dart
+++ lib/feature/auth/data/repositories/authentication_repository.dart
@@ -18,9 +18,12 @@ class AuthenticationRepository implements IAuthenticationRepository {
   }
 
   @override
-  Future<void> signInWithEmail({required String email}) {
-    // TODO: implement signInWithEmail
-    throw UnimplementedError();
+  Future<void> signInWithEmail(
+      {required String email, required String password}) async {
+    await supabase.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
   }
 
   @override
--- lib/feature/auth/domain/repositories/i_authentication_repository.dart
+++ lib/feature/auth/domain/repositories/i_authentication_repository.dart
@@ -1,7 +1,8 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 abstract interface class IAuthenticationRepository {
-  Future<void> signInWithEmail({required String email});
+  Future<void> signInWithEmail(
+      {required String email, required String password});
   Future<void> signOut();
   Stream<User?> getCurrentUser();
   User? getSignedInUser();
