b44bd828a33eb9ae0e5ec222f3d6e612487efdc1
MeZeksan <bulzak74848@gmail.com>
2025-03-26 23:13:43 +0300

    Получение клиента supabase

--- lib/feature/auth/data/repositories/authentication_repository.dart
+++ lib/feature/auth/data/repositories/authentication_repository.dart
@@ -3,6 +3,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../domain/repositories/i_authentication_repository.dart';
 
 class AuthenticationRepository implements IAuthenticationRepository {
+  final supabase = Supabase.instance.client; //ссылка на клиент supabase
+
   @override
   Stream<User?> getCurrentUser() {
     // TODO: implement getCurrentUser
