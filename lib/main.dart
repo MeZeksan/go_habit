ca976d6f5472bfd681e02cd9580c070f568eb594
MeZeksan <bulzak74848@gmail.com>
2025-03-27 15:26:21 +0300

    Добавил обращение к секретам через пакет dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:go_habit/core/environment/.env_config.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'core/environment/env_config.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Загрузка переменных окружения
+  await dotenv.load(fileName: ".env");
 
   //supabase init
   await Supabase.initialize(
