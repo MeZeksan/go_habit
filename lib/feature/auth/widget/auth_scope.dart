c34ab1a761fe00e9e23661817d73bb87f1667b0a
MeZeksan <bulzak74848@gmail.com>
2025-03-28 14:34:58 +0300

    изменения логики получение AuthenticationRepository с помощью DI через yx_scope

--- lib/feature/auth/widget/auth_scope.dart
+++ lib/feature/auth/widget/auth_scope.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_habit/feature/auth/data/repositories/authentication_repository.dart';
 import 'package:go_habit/feature/auth/domain/bloc/auth_bloc.dart' as app_auth;
-import 'package:go_habit/feature/initizialization/scopes/app_scope_container.dart';
 import 'package:yx_scope_flutter/yx_scope_flutter.dart';
+
+import '../../initizialization/scopes/app_scope_container.dart';
 
 class AuthScope extends StatelessWidget {
   final Widget child;
@@ -12,13 +12,15 @@ class AuthScope extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScopeBuilder<AppScopeContainer>(
-      builder: (context, appScope) => BlocProvider<app_auth.AuthBloc>(
+    //withPlaceholder добавляет проверку на null
+    return ScopeBuilder<AppScopeContainer>.withPlaceholder(
+        builder: (context, scope) {
+      return BlocProvider<app_auth.AuthBloc>(
         create: (context) => app_auth.AuthBloc(
-          AuthenticationRepository(),
+          scope.authRepositoryDep.get,
         )..add(app_auth.AuthCheckRequested()),
         child: child,
-      ),
-    );
+      );
+    });
   }
 }
